d0c4cd5a5f1e8c6ec39115ed5b93caab86889637
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-08-28 23:33:49 +0300

    fixing the folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import SendNotification from "./components/SendNotification/page";
+import SendNotification from "@/components/SendNotification/page";
 
 export default function Home() {
   return (
